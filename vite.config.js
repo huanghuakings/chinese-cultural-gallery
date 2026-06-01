5b9ce55e183ebeb3e1c859fa4066be5790d0c3ba
huang hua <huanghuakings@163.com>
2026-06-02 01:53:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/你的仓库名/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/chinese-cultural-gallery/' : '/',
   server: {
     port: 3000,
     open: true
